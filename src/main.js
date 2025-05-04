7d903eb6f5e31ab8c349d981bbec4330de07583e
ALIAFAR <alifarvazova@gmail.com>
2025-05-04 23:24:11 +0500

    Revert "password visibility"
    This reverts commit ea9040677e58e00810bdee53d01cf631c5ae4e8f.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import Notifications from '@kyvg/vue3-notification'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faEye, faEyeSlash)
-
-app.component('font-awesome-icon', FontAwesomeIcon)
 
 const app = createApp(App);
 
